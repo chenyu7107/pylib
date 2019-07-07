f0663a89b20aeae81e89ce96f0dceeb70ba8aa83
unknown <chenyu7107@163.com>
2019-07-07 11:41:51 +0800

    gg

---
+++ src/test/test.py
@@ -0,0 +1,10 @@
+import sys
+#sys.path.append("..")
+from img.A import *
+#from B import *
+from B import *
+if __name__=='__main__':
+    size = len(sys.argv)
+    add(1,2)
+    wcy()
+    print('main')
